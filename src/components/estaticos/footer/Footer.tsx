2bea6f1ce89a1d5371448ba4e635382da6e1e986
VinnyPC <vinisilvax3@gmail.com>
2023-05-15 15:50:52 -0300

    estilização da pagina home

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -3,14 +3,15 @@ import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import {Typography, Grid } from '@material-ui/core';
+import './Footer.css';
 import {Box} from '@mui/material';
 
 function Footer() {
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center">
+            <Grid container direction="row" justifyContent="center" alignItems="center" className=''>
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
+                    <Box style={{ backgroundColor: "#810081", height: "120px" }}>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
                             <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
                         </Box>
@@ -26,7 +27,7 @@ function Footer() {
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
+                    <Box style={{ backgroundColor: "#810081", height: "60px" }}>
                         <Box paddingTop={1}>
                             <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
                         </Box>
--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,47 +1,56 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import './Navbar.css'
+import { AppBar, Divider, Toolbar, Typography } from '@material-ui/core';
 import {Box} from '@mui/material';
 function Navbar() {
     return (
         <>
-            <AppBar position="static">
-                <Toolbar variant="dense">
+            <AppBar position="static" >
+                <Toolbar variant="dense" style={{background: "#810081"}} className='Navbar'>
                     <Box style={{ cursor: "pointer" }} >
-                        <Typography variant="h5" color="inherit">
-                            BlogPessoal
+                        <Typography variant="h5" color="inherit" >
+                            BlogPessoal - Vinicius
                         </Typography>
+                        
                     </Box>
-
-                    <Box display="flex" justifyContent="start">
+                    
+                    <Box display="flex" justifyContent="start" gap={{sm: 6}} marginLeft={6}>
+                    <Divider orientation="vertical" flexItem/>
                         <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
+                            <Typography variant="h6" color="inherit" className='MenuBtn' style={{}}>
                                 Home
                             </Typography>
                         </Box>
+                        <Divider orientation="vertical" flexItem/>
                         <Box mx={1} style={{ cursor: "pointer" }}>
                             <Typography variant="h6" color="inherit">
                                 Postagens
                             </Typography>
                         </Box>
+                        <Divider orientation="vertical" flexItem/>
                         <Box mx={1} style={{ cursor: "pointer" }}>
                             <Typography variant="h6" color="inherit">
                                 Temas
                             </Typography>
                         </Box>
+                        <Divider orientation="vertical" flexItem/>
                         <Box mx={1} style={{ cursor: "pointer" }}>
                             <Typography variant="h6" color="inherit">
                                 Cadastrar Tema
                             </Typography>
                         </Box>
+                        <Divider orientation="vertical" flexItem/>
                         <Box mx={1} style={{ cursor: "pointer" }}>
                             <Typography variant="h6" color="inherit">
                                 Logout
                             </Typography>
                         </Box>
+                        <Divider orientation="vertical" flexItem/>
                     </Box>
 
                 </Toolbar>
             </AppBar>
+            
         </>
     )
 }
